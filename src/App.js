35a0f7d07b143c2a2038690a294238a7ccbb037c
Pedro Henrique <pedro.andrade@nave.org.br>
2022-09-27 14:41:07 -0300

    publish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {useForm} from 'react-hook-form';
+
 
 function App() {
+  
+  const {register, handleSubmit, errors} = useForm();
+  
+  const OnSubmit = (data) => {
+    console.log(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <form onSubmit={handleSubmit(OnSubmit)}>
+      <input type= "text" placeholder="Nome" name= "Nome"  {...register('nome', { required: true })}/>
+      <input type= "text" placeholder="Sobrenome" name= "Sobrenome"  {...register('sobrenome', { required: true })}/>
+      <input type= "text" placeholder= "CPF" name= "cpf" {...register('cpf', { required: true })}/>
+      <input type= "text" placeholder= "CEP" name= "cep" {...register('cep', { required: true })}/>
+      <input type= "text" placeholder= "Endereço" name= "endereco" {...register('endereco', { required: true })}/>
+      <input type= "text" placeholder= "Número" name= "numero" {...register('numero', { required: true })}/>
+      <input type= "password" placeholder= "Senha" name= "password" {...register('senha', { required: true })}/>
+      <input type= "password" placeholder= "ConfirmarSenha" name= "passwordconf" {...register('senhaconf', { required: true } )}/>
+      
+      <input type= "submit" />
+
+   </form>
   );
 }
 
